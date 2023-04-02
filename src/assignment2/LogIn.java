d15a289f5b0e5d2e5748b1e10902a6845583451d
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-02 15:34:11 -0400

    adding modified file

--- src/assignment2/LogIn.java
+++ src/assignment2/LogIn.java
@@ -2,7 +2,7 @@ package assignment2;
 import java.util.Scanner;
 public class LogIn {
 	
-	String userId = "pivotadmin";
+	String userId = "pivotAdmin";
 	String password = "Admin123";
 	String enteredUserId;
 	String enteredPassword;
@@ -16,22 +16,16 @@ public class LogIn {
 				counter++) {
 			System.out.println("enter the userId");
 			enteredUserId = sc.next();
-			if(enteredUserId == userId) {
-				System.out.println("entered userId is correct");
-				System.out.println("enter the password");
-				enteredPassword = sc.next();
-				if(enteredPassword == password) {
-					System.out.println("entered password is correct, logged in");
+			System.out.println("enter the password");
+			enteredPassword = sc.next();
+			if(enteredUserId.equals(userId) && enteredPassword.equals(password)) {
+				System.out.println("you are logged into the application");
 					break;
-				}
-			}else if(enteredUserId == userId && enteredPassword != password){
-				System.out.println("entered password is incorrect, try again");
-			}else if (counter ==3 && enteredUserId != userId) {
-				System.out.println("account is locked");	
+				}else{
+				System.out.println("incorrect user Id or password, try again");
+			}if ((counter ==3 && enteredPassword != password) || (counter == 3 && enteredUserId != userId)) {
+				System.out.println("account is locked");
 			}
-		else {
-			System.out.println("entered userId is incorrect");
-		}
 	}
 	
 	}
