8e4b12dcd2b3f9ff37cfd0022f18f8282a0f1b7a
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-02 15:31:30 -0400

    adding modified file

--- src/assignment2/CalculatorMain.java
+++ src/assignment2/CalculatorMain.java
@@ -1,10 +1,37 @@
 package assignment2;
-
+import java.util.Scanner;
 public class CalculatorMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String operation;
+		Scanner sc = new Scanner(System.in);
+		Calculator calc =  new Calculator();
+		System.out.println("enter the operation");
+		operation = sc.next();
+		
+		switch(operation) {
+		case "addition":
+			calc.addition();
+			break;
+		case "substarction":
+			calc.substraction();
+			break;
+		case "multiplication":
+			calc.multiplication();
+			break;
+		case "division":
+			calc.division();
+			break;
+		case "square":
+			calc.squareOfNumber();
+			break;
+		case "cube":
+			calc.cubeOfNumber();
+			break;
+			default:
+				System.out.println("invalid operation");
+		}
 	}
 
 }
