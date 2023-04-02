b31ec5a79eb65f93ab2ce60b8a853528a682924b
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-02 15:38:19 -0400

    adding file

--- src/learnLoops/ForLoop.java
+++ src/learnLoops/ForLoop.java
@@ -18,21 +18,27 @@ public class ForLoop {
 				counter++) {
 			System.out.println("Enter the PIN");
 			enteredPIN = sc.nextInt();
-			System.out.println("Enter the withdraw amount");
-			withdrawAmount = sc.nextInt();
-			if (actualPIN == enteredPIN && withdrawAmount<=accountBalance) {
-				System.out.println("Entered PIN is correct");
-				System.out.println("Cash withdrawn amount is: "+withdrawAmount);
-				break;
-			}
-		else if(counter == 3 && actualPIN != enteredPIN){
+	 if(enteredPIN == actualPIN) {
+		 System.out.println("entered PIN is correct");
+	 }
+	 System.out.println("Enter the withdraw amount");
+		withdrawAmount = sc.nextInt();
+	 if (withdrawAmount<=accountBalance) {
+		 System.out.println("amount withdraw is" + withdrawAmount);
+	 }else {
+		 System.out.println("not enough balance");
+		 break;
+	 }
+		if(counter == 3 && actualPIN != enteredPIN){
 				System.out.println("Card is blocked as three attempts for PIN is incorrect");
 			}else {
 				System.out.println("Entered PIN is incorrect");
 			}
 		}
-	}
+	
+		}
 
 }
+
 
 
