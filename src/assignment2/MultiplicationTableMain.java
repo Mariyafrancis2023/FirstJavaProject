0423e8b70a7393d9fab5184d80c883f9abeae2e2
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-04 19:10:58 -0400

    adding a file

---
+++ src/assignment2/MultiplicationTableMain.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+public class MultiplicationTableMain {
+
+	public static void main(String[] args) {
+		
+		MultiplicationTable multiply = new MultiplicationTable();
+		
+		multiply.printMultiple();
+
+	}
+
+}
