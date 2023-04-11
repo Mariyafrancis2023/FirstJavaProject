036885382039c186d02683d6fed99229a7f2dc3f
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-11 14:10:16 -0400

    modified file

--- src/learnStatements/DrivingLicenseMain.java
+++ src/learnStatements/DrivingLicenseMain.java
@@ -5,8 +5,9 @@ public class DrivingLicenseMain {
 	public static void main(String[] args) {
 
 		Drivinglicense dl = new Drivinglicense();
-		dl.age = 10;
-		dl.isLevelG1 = true;
+		dl.age = 16;
+	//	dl.isLevelG1 = true;
+	//	dl.isLevelG2 = true;
 		dl.typeOfLicence();
 
 	}
