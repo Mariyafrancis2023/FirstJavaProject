8ff2197514cf5837ed2faef6b0920a28c7c64abc
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-20 17:30:04 -0400

    add edited file

--- src/learnString/StringDemoMain.java
+++ src/learnString/StringDemoMain.java
@@ -7,12 +7,30 @@ public class StringDemoMain {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringDemo demo = new StringDemo();
-		demo.name = "qwertyuiop";
+		
+		demo.name = "abcdefghabcdeabca";
+		
 		System.out.println("enter the char");
 		String charBeCheck = sc.next();
 		demo.checkACharactor(charBeCheck);
 		
-
+		int count = demo.numberOfTimeACharOccur('a');
+		System.out.println("no: of time char present is "+count);
+		
+		String lastName = demo.findLastName(8);
+		System.out.println("the substring is "+lastName);
+		
+//		String cityWithTon = demo.findCityWithTon();
+//		System.out.println(cityWithTon);
+		
+		boolean cityFound = demo.findCityName("windsor");
+		if(cityFound) {
+			System.out.println("city found");
+		}else {
+			System.out.println("city not found");
+		}
+		
+//		demo.findCityName(enterCityName);
 	}
 
 }
