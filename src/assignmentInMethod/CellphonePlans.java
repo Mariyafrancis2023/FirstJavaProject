f2027e650589814ad6d0a0196008e33679f1c5dd
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-19 21:18:23 -0400

    add file

--- src/assignmentInMethod/CellphonePlans.java
+++ src/assignmentInMethod/CellphonePlans.java
@@ -10,69 +10,52 @@ public class CellphonePlans {
 	int[] applePhonePrice = { 30, 40, 65, 25 };
 	String[] planProvider = { "Rogers", "Bell", "Telus" };
 	int[] planPrice = { 70, 80, 75 };
-	String android = "Samsung";
-	String ios = "Apple";
-	int samsungPhoneTab;
-	int applePhoneTab;
+	String enteredPhoneName;
+	int phoneTabPrice;
 	int phonePlanPrice;
 	int monthlyAmount;
+	int i=0;
 
 	Scanner sc = new Scanner(System.in);
 
-	void phoneProductsAvailable(String enteredBrandName) {
-		System.out.println("enter the model you want to buy from below");
-		if (enteredBrandName.equals(android)) {
-			for (int i = 0; i < samsungPhoneModel.length; i++) {
-				System.out.println(samsungPhoneModel[i]);
-			}
-		} else if (enteredBrandName.equals(ios)) {
-			for (int i = 0; i < applePhoneModel.length; i++) {
-				System.out.println(applePhoneModel[i]);
+	boolean getSamsungPhonesAndPrice() {
+		boolean isenteredPhoneName = true;
+		System.out.println("enter the available samsung model");
+	    enteredPhoneName = sc.next();
+	    for (int i = 0; i < samsungPhoneModel.length; i++) {
+	    	if (enteredPhoneName.equals(applePhoneModel[i])) {
+				System.out.println("phone tab will be " + samsungPhonePrice[i]);
+				phoneTabPrice = samsungPhonePrice[i];
 			}
 		}
-	}
-	void phoneModelPrices(String enterModelName) {
-		for (int i = 0; i < samsungPhoneModel.length; i++) {
-		if(enterModelName.equals(samsungPhoneModel[i])) {
-			System.out.println("phone tab will be: " +samsungPhonePrice[i]);
-			samsungPhoneTab=samsungPhonePrice[i];
-			break;
-		}			
+	    return isenteredPhoneName;
 		}
+		
+	void getApplePhonesAndPrice() {
+		System.out.println("enter the available apple model");
+		enteredPhoneName = sc.next();
 		for (int i = 0; i < applePhoneModel.length; i++) {
-			if(enterModelName.equals(applePhoneModel[i])) {
-				System.out.println("phone tab will be: " +applePhonePrice[i]);	
-				applePhoneTab=applePhonePrice[i];
-				break;
-			}			
-			}
-	}
-/*	void applePhoneAvailable(String enteredBrandName) {
-		if (enteredBrandName.equals(ios)) {
-			System.out.println("enter the model you want to buy from below");
-			for (int i = 0; i < applePhoneModel.length; i++) {
-				System.out.println(applePhoneModel[i]);
+			if (enteredPhoneName.equals(applePhoneModel[i])) {
+				System.out.println("phone tab will be " + applePhonePrice[i]);
+				phoneTabPrice = applePhonePrice[i];
 			}
 		}
-	}    */
+	}
 
-	void planProviderAvailable(String enteredProviderName) {
+	void getPlanProviderAvailable() {
+		System.out.println("select the plan you want to opt for: Rogers/Telus/Bell");
+		String enterProviderName = sc.next();
 		for (int i = 0; i < planProvider.length; i++) {
-			if (enteredProviderName.equals(planProvider[i])) {
-				System.out.println("monthly plan will be: " +planPrice[i]);	
-				phonePlanPrice=planPrice[i];
+			if (enterProviderName.equals(planProvider[i])) {
+				System.out.println("monthly plan will be: " + planPrice[i]);
+				phonePlanPrice = planPrice[i];
 				break;
 			}
 		}
 	}
-	
-	void monthlyAmountForSamsungPhone() {
-		int monthlyAmount = samsungPhoneTab + phonePlanPrice;
-		System.out.println("total monthly amount: "+monthlyAmount);
-	}
-	void monthlyAmountForApplePhone() {
-		int monthlyAmount = applePhoneTab + phonePlanPrice; 
-		System.out.println("total monthly amount: "+monthlyAmount);
-	}  
 
+	void calculateMonthlyPlanAmount() {
+		monthlyAmount = phoneTabPrice + phonePlanPrice;
+		System.out.println("total monthly amount: " + monthlyAmount);
+	}
 }
