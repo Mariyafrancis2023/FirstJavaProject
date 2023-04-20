16ff84f150e14e78cbcea7d1347eac75533dfeb9
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-20 17:29:17 -0400

    add edited file

--- src/learnString/StringDemo.java
+++ src/learnString/StringDemo.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class StringDemo {
 	String name;
+	String[] cities = {"Toronto","Ottawa","Brampton","Hamilton","London","Mississauga","Burlington","Kingston","Kitchener"};
+	int i=0;
 	Scanner sc = new Scanner(System.in);
 	
+/////////////////////////FIND IF A CHAR IS PRESENT ON A STRING////////////////////////	
+	
 	void checkACharactor(String charBeCheck){	
-	/*	 name = "asdfghjkl";
+	/*	 name = "abcdefghijklmnop";
 		 if(name.contains("g")) {
 			 System.out.println("char is found"); 		 
 		 }else {
@@ -19,5 +23,63 @@ public class StringDemo {
 			 System.out.println(charBeCheck+ " is not found");
 		 }
 	}
+	
+///////////////////FIND HOW MANY TIME A CHAR IS PRESENT ON A STRING///////////////////
+	
+	int numberOfTimeACharOccur (char charToChcek) {
+		int counter=0;
+		for(int i=0;i<name.length();i++) {
+			if(name.charAt(i)==charToChcek) {
+				counter++;
+			}		
+		}
+		return counter;
+	}
+	
+//////////////////////////////FIND A SUBSTRING FROM A STRING//////////////////////////
+	
+	String findLastName(int indexToBeUsed) {
+		String lastName = name.substring(indexToBeUsed);
+		return lastName;
+	}
+	
+//////////////////////////////////////////////////////////////////////////////////////
+	
+	String findCityWithTon() {
+		String[] citiesWithTon = new String[5];
+		System.out.println("city ending with ton is:");
+		for(int i=0;i<cities.length;i++) {
+			if(cities[i].endsWith("ton")) {
+				citiesWithTon[i] = cities[i];
+			}
+		}
+		return citiesWithTon[i];
+	}
+	
+//////////////////////////////////////////////////////////////////////////////////////
+	
+	boolean findCityName(String enterCityName) {
+		boolean IsCityFound = false;
+		for(int i=0;i<cities.length;i++) {
+		if(enterCityName.equals(cities[i])) {
+				IsCityFound = true;
+			}
+		}
+		return IsCityFound;
+	}
+	
+	
+/*	void findCityName(String enterCityName) {
+	for(int i=0;i<cities.length;i++) {
+		if(enterCityName.equals(cities[i])) {
+				System.out.println("city is found");
+				break;
+			}
+		}
+		int i=0;
+		if (enterCityName!=cities[i]) {
+			System.out.println("city is not found");
+			i++;
+		}  */
 
 }
