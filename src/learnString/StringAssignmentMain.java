ad08546d64d4582cf2b3b5d887f825c310767c36
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-04-20 17:28:20 -0400

    add edited file

--- src/learnString/StringAssignmentMain.java
+++ src/learnString/StringAssignmentMain.java
@@ -7,11 +7,21 @@ public class StringAssignmentMain {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		StringAssignment city = new StringAssignment();
+		
 		System.out.println("enter the city name");
 		String enterCityName = sc.next();
-		city.findCityName(enterCityName);
+		
+		boolean cityFound = city.findCityName(enterCityName);
+		if(cityFound) {
+			System.out.println("city found");
+		}else {
+			System.out.println("city not found");
+		}
+
 		city.findCityWithTon();
+		
 		city.replacingTonWithFun();
+		
 		city.splitingAddress();	
 		}
 	}
