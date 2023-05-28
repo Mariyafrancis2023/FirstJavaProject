9047d8fb322324ddda20ffe9e709ed9598162d86
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-05-28 17:56:29 -0400

    add file

--- src/abstractionAssignment/ATM.java
+++ src/abstractionAssignment/ATM.java
@@ -4,78 +4,54 @@ import java.util.Scanner;
 
 public class ATM extends BankOperation {
 
-	private PersonAccount[] person = new PersonAccount[5];
-
-	public ATM(PersonAccount[] person) {
-		this.person = person;
-	}
-
 	Scanner sc = new Scanner(System.in);
 
 	@Override
-	public void changePinPassword() {
+	public void changePinPassword(PersonAccount account) {
 		System.out.println("enter the current pin");
 		String currentPin = sc.next();
-		boolean isPinFound = false;
-		for (int i = 0; i < person.length; i++) {
-			if (currentPin.equals(person[i].getPin())) {
-				System.out.println("enter new pin");
-				String newPin = sc.next();
-				if (!newPin.equals(currentPin) && newPin.length() == 4) {
-					person[i].setPin(newPin);
-					System.out.println("pin is updated");
-					isPinFound = true;
-					break;
-				} else {
-					System.out.println("updated pin cannot the same as old pin and pin should have 4 numbers");
-					isPinFound = true;
-					break;
-				}
+		if (currentPin.equals(account.getPin())) {
+			System.out.println("enter new pin");
+			String newPin = sc.next();
+			if (newPin != currentPin && newPin.length() == 4) {
+				account.setPin(newPin);
+				System.out.println("pin is updated");
+			} else {
+				System.out.println("pin should have 4 digits");
 			}
-		}
-		if (!isPinFound) {
-			System.out.println("incorrect pin");
+		} else {
+			System.out.println("updated pin cannot the same as old pin");
 		}
 	}
 
 	@Override
-	public void withdraw(double withdrawAmount) {
+	public void withdraw(PersonAccount account, double withdrawAmount) {
 		System.out.println("enter your pin number");
 		String enteredPin = sc.next();
-		for (int i = 0; i < person.length; i++) {
-			if (enteredPin.equals(person[i].getPin())) {
-				if (person[i].getFundsAvailable() >= withdrawAmount) {
-					super.withdraw(withdrawAmount);
-					System.out.println(withdrawAmount + " is withdrawn");
-				} else {
-					System.out.println("not enough balance");
-					break;
-				}
-			}
+		if (enteredPin.equals(account.getPin())) {
+			super.withdraw(account, withdrawAmount);
 		}
 	}
 
 	@Override
-	public void deposit(double depositMoney) {
+	public void deposit(PersonAccount account, double depositMoney) {
 		System.out.println("enter your pin number");
 		String enteredPin = sc.next();
-		for (int i = 0; i < person.length; i++) {
-			if (enteredPin.equals(person[i].getPin())) {
-				super.deposit(depositMoney);
-				System.out.println(depositMoney + " is deposited");
-			}
+		if (enteredPin.equals(account.getPin())) {
+			super.deposit(account, depositMoney);
+			System.out.println(depositMoney + " is deposited");
+			account.setFundsAvailable(account.getFundsAvailable() + depositMoney);
+			System.out.println("now the avaliable balance is " + account.getFundsAvailable());
 		}
 	}
 
 	@Override
-	public double viewBalance() {
+	public double viewBalance(PersonAccount account) {
 		System.out.println("enter your pin number");
 		String enteredPin = sc.next();
-		for (int i = 0; i < person.length; i++) {
-			if (enteredPin.equals(person[i].getPin())) {
-				balanceAvailable = person[i].getFundsAvailable();
-				System.out.println("the available balance is " + balanceAvailable);
-			}
+		if (enteredPin.equals(account.getPin())) {
+			balanceAvailable = account.getFundsAvailable();
+			System.out.println("the available balance is " + balanceAvailable);
 		}
 		return balanceAvailable;
 	}
