3b697dac86c088abeb5a7b44a6dadd4522321ee0
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-05-28 17:59:49 -0400

    add file

--- src/abstractionAssignment/OnlineBanking.java
+++ src/abstractionAssignment/OnlineBanking.java
@@ -4,68 +4,44 @@ import java.util.Scanner;
 
 public class OnlineBanking extends BankOperation {
 
-	private PersonAccount[] person = new PersonAccount[5];
-
-	public OnlineBanking(PersonAccount[] person) {
-		this.person = person;
-	}
-
 	Scanner sc = new Scanner(System.in);
 
 	@Override
-	public void changePinPassword() {
+	public void changePinPassword(PersonAccount account) {
 		System.out.println("enter the current password");
 		String currentPassword = sc.next();
-		boolean isPasswordFound = false;
-		for (int i = 0; i < person.length; i++) {
-			if (currentPassword.equals(person[i].getOnlineBankingPassword())) {
-				System.out.println("enter new password");
-				String newPassword = sc.next();
-				if (newPassword != currentPassword && newPassword.length() >= 8 && !newPassword.contains("&")
-						&& !newPassword.contains("$") && !newPassword.contains("@")) {
-					person[i].setOnlineBankingPassword(newPassword);
-					System.out.println("password is updated");
-					isPasswordFound = true;
-					break;
-				} else {
-					System.out.println(
-							"passowrd should not be less than 8 characters and have these special characters (&,$, @)");
-					isPasswordFound = true;
-					break;
-				}
+		if (currentPassword.equals(account.getOnlineBankingPassword())) {
+			System.out.println("enter new password");
+			String newPassword = sc.next();
+			if (newPassword != currentPassword && newPassword.length() >= 8 && !newPassword.contains("&")
+					&& !newPassword.contains("$") && !newPassword.contains("@")) {
+				account.setOnlineBankingPassword(newPassword);
+				System.out.println("password is updated");
+			} else {
+				System.out.println(
+						"passowrd should not be less than 8 characters and have these special characters (&,$, @)");
 			}
-		}
-		if (!isPasswordFound) {
-			System.out.println("incorrect password");
+		} else {
+			System.out.println("updated password cannot the same as old password");
 		}
 	}
 
 	@Override
-	public void withdraw(double withdrawAmount) {
+	public void withdraw(PersonAccount account, double withdrawAmount) {
 		System.out.println("enter your password");
 		String enteredPassword = sc.next();
-		for (int i = 0; i < person.length; i++) {
-			if (enteredPassword.equals(person[i].getOnlineBankingPassword())) {
-				if (person[i].getFundsAvailable() >= withdrawAmount) {
-					super.withdraw(withdrawAmount);
-					System.out.println(withdrawAmount + " is withdrawn");
-				} else {
-					System.out.println("not enough balance");
-					break;
-				}
-			}
+		if (enteredPassword.equals(account.getOnlineBankingPassword())) {
+			super.withdraw(account, withdrawAmount);
 		}
 	}
 
 	@Override
-	public double viewBalance() {
+	public double viewBalance(PersonAccount account) {
 		System.out.println("enter your password");
 		String enteredPassword = sc.next();
-		for (int i = 0; i < person.length; i++) {
-			if (enteredPassword.equals(person[i].getOnlineBankingPassword())) {
-				balanceAvailable = person[i].getFundsAvailable();
-				System.out.println("the available balance is " + balanceAvailable);
-			}
+		if (enteredPassword.equals(account.getOnlineBankingPassword())) {
+			balanceAvailable = account.getFundsAvailable();
+			System.out.println("the available balance is " + balanceAvailable);
 		}
 		return balanceAvailable;
 	}
