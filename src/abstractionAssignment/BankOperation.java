f26fc0781fc1c9daa0a8edfd235919c20c14893e
Mariyafrancis2023 <mariapadayatty@gmail.com>
2023-05-28 17:58:31 -0400

    add file

--- src/abstractionAssignment/BankOperation.java
+++ src/abstractionAssignment/BankOperation.java
@@ -1,21 +1,26 @@
 package abstractionAssignment;
 
-	public abstract class BankOperation {
-		
-		public double balanceAvailable;
-		
-		public abstract void changePinPassword(); 
-		
-		public void withdraw(double withdrawAmount) {
-				balanceAvailable -= withdrawAmount;			
-		}
+public abstract class BankOperation {
 
-		public void deposit(double depositMoney) {
-			balanceAvailable += depositMoney;
-		}
+	public double balanceAvailable;
 
-		public double viewBalance() {
-			return balanceAvailable;	
+	public abstract void changePinPassword(PersonAccount account);
+
+	public void withdraw(PersonAccount account, double withdrawAmount) {
+		if (account.getFundsAvailable() >= withdrawAmount) {
+			System.out.println(withdrawAmount + " is withdrawn");
+			account.setFundsAvailable(account.getFundsAvailable() - withdrawAmount);
+			System.out.println("now the avaliable balance is " + account.getFundsAvailable());
+		} else {
+			System.out.println("not enough balance");
 		}
 	}
 
+	public void deposit(PersonAccount account, double depositMoney) {
+		balanceAvailable += depositMoney;
+	}
+
+	public double viewBalance(PersonAccount account) {
+		return balanceAvailable;
+	}
+}
